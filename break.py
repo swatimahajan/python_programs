40b6da40d033df9164d1023b579064ce24b479a3
swatimahajan <mahajanswati108@gmail.com>
2015-06-16 13:13:02 +0530

    Update break.py

--- break.py
+++ break.py
@@ -2,7 +2,9 @@ import time
 import webbrowser
 i =0
 print("this program started on" + time.ctime())
+#repeat this process 3 times
 while i <=3:
+#open google page after every 5 sec
     time.sleep(5)
     webbrowser.open("https://www.google.com")
     i=i+1
