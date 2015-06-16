618ce14c92b1f0eb5bc2687fba0725a6b66912b6
swatimahajan <mahajanswati108@gmail.com>
2015-06-16 16:18:13 +0530

    program to build  number triangle

---
+++ triangle.py
@@ -0,0 +1,4 @@
+
+for i in range(1,input("enter a number: ")):
+    print i*(int(pow(10,i-1))+int(pow(10,i-2))+int(pow(10,i-3))+int(pow(10,i-4))+int(pow(10,i-5))+int(pow(10,i-6))+int(pow(10,i-7))+int(pow(10,i-8))+int(pow(10,i-9)))
+
