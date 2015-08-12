217e841bc67a6b987f09d2da0f786eb42e6ea0f6
swatimahajan <mahajanswati108@gmail.com>
2015-08-12 15:16:08 +0530

    program to find unique elements between 2 lists

---
+++ set1.py
@@ -0,0 +1,13 @@
+def user_input():
+    input()
+    l = raw_input()
+    result = l.split()
+# map converts input string list into int
+    r= list(map(int,result))
+    return r
+M = user_input()
+N =user_input()
+print M
+print N
+# to print unique elements between 2 lists M and N
+print list(set(M).difference(N))+ list(set(N).difference(M))
