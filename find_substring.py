e170b34a3f6d7101ed3b2c1b8b34398d4533cd5d
swatimahajan <mahajanswati108@gmail.com>
2015-08-17 13:32:09 +0530

    Update find_substring.py

--- find_substring.py
+++ find_substring.py
@@ -1,10 +1,11 @@
 def occurrences(string, substring):
+	l = len(string)
 	count = start = 0
-	while True:
+	for i in range(l):
 		start = string.find(substring, start) + 1
 		if start > 0:
 			count+=1
 		else:
 			return count
-print occurrences(raw_input().upper(),raw_input().upper())
+print occurrences(raw_input(),raw_input())
 
